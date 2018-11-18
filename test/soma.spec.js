b324bd11f34f2ead1e7784ab5917db80868d6d66
shehur <shehur5727@gmail.com>
2018-11-18 15:44:35 -0200

    quebra da função somar

--- test/soma.spec.js
+++ test/soma.spec.js
@@ -2,5 +2,5 @@ const { expect } = require('code');
 const { it } = exports.lab = require('lab').script();
 
 it('2 + 3 = 5', () => {
-	expect(2 + 3).to.equal(5);
+	expect(somar(2, 3)).to.equal(5);
 });
